c33a6f0d628e8db56e1f91a81a26a244d9212bf0
Joe MacNish <wmacnish@gmail.com>
2025-03-20 09:32:39 -0700

    Updating robots handling

--- templates/header.php
+++ templates/header.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Portfolio of William MacNish - Front End Web Developer, etc.</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
     <script src="/assets/scripts/script.js" type="module"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
